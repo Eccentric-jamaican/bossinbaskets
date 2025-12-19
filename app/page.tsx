826ff4ea7aba2d8acc2d49ffda44b290425ef50a
Eccentric-jamaican <aellisatl@gmail.com>
2025-12-18 21:35:23 -0500

    Prevent mobile drawer jitter on home page

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Newsletter from "@/components/site/Newsletter";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col font-sans">
+    <div className="flex min-h-screen flex-col overflow-x-hidden font-sans">
       <Nav />
       <main className="flex-1">
         <Hero />
